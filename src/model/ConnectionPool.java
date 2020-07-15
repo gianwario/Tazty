173642bcdd63ef0c39f7f76f59ef40013b57306f
BearGrillz99 <65906352+BearGrillz99@users.noreply.github.com>
2020-07-15 18:08:58 +0200

    Bean, Dao, Add di tutto

--- src/model/ConnectionPool.java
+++ src/model/ConnectionPool.java
@@ -3,8 +3,6 @@ package model;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.LinkedList;
-import java.util.List;
 
 public class ConnectionPool {
 
